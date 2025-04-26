909b75d4164f12898e099609639ade949ab295ea
SherieHub <guadianasherielyn25@gmail.com>
2025-04-26 11:52:13 +0800

    FINAL SETUP of the background image

--- CookingIna/src/main/java/com/example/cookingina/Main.java
+++ CookingIna/src/main/java/com/example/cookingina/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1000, 1000);
+        Scene scene = new Scene(fxmlLoader.load(), 1920, 1080);
         stage.setTitle("Cooking Ina: Ang Sarap!");
         stage.setScene(scene);
         stage.show();
